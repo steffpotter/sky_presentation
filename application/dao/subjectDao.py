451584caa96392075aff0bc41355f76e44cd1a14
Steff Potter <steff@hopeandpotter.co.uk>
2023-04-10 21:02:24 +0100

    Merge to fix conflicts

--- application/dao/subjectDao.py
+++ application/dao/subjectDao.py
@@ -4,6 +4,8 @@ from question import Question
 from answer import Answer
 
 
+
+
 class SubjectDao(BaseDao):
 
     """
@@ -16,7 +18,8 @@ class SubjectDao(BaseDao):
 
     def getAllSubjects(self):
         """ Returns a list of Subject objects."""
-        subjects = self._db.getAllSubjects()  # returns list of dictionaries corresponding to Subject rows in db
+        subjects = self._db.getAllSubjects()
+        # returns list of dictionaries corresponding to Subject rows in db
         # map subject dictionaries to Subject Python object using a list comprehension
         subjectObjs = [Subject(subject.get("subject_id"),
                                subject.get("subject_name"),
@@ -33,15 +36,20 @@ class SubjectDao(BaseDao):
 
     def getSubject(self, subjectId):
         subject = self._db.getSubject(subjectId)  # todo - test how mysql connector handles error when no subject found
-        return Subject(subjectId,
-                       subject.get("subject_name"),
-                       subject.get("subject_logo"),
-                       subject.get("subject_content"),
-                       [Question(question.get("question_id"), 
-                                 question.get("question_text"), 
-                                 question.get("correct_answer_id"),
-                                 [Answer(answer.get("answer_id"), 
-                                         answer.get("answer_text")) for answer in question.get("answers")]) 
-                                 for question in subject.get("subject_questions")])
+        
+        try:
+            return Subject(subjectId,
+                        subject.get("subject_name"),
+                        subject.get("subject_logo"),
+                        subject.get("subject_content"),
+                        [Question(question.get("question_id"), 
+                                    question.get("question_text"), 
+                                    question.get("correct_answer_id"),
+                                    [Answer(answer.get("answer_id"), 
+                                            answer.get("answer_text")) for answer in question.get("answers")]) 
+                                    for question in subject.get("subject_questions")])
+        except Exception as e:
+            print("Error in getSubjectByName: ", e)
+
 
     # Can extend to have Add, Update and Delete functions below
